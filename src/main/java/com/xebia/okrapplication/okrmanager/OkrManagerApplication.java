4a981733aa9f91825107cae06bfb04c62d167bb4
preetitheraja <preeti.theraja@xebia.com>
2019-08-14 13:12:52 +0530

    Spring Boot Files

---
+++ src/main/java/com/xebia/okrapplication/okrmanager/OkrManagerApplication.java
@@ -0,0 +1,13 @@
+package com.xebia.okrapplication.okrmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkrManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkrManagerApplication.class, args);
+	}
+
+}
